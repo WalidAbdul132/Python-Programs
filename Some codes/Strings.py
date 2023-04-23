1b900e51ca9abfa973939a4d88aa6f6e5a8d0755
WalidAbdul132 <abdulbuster132@gmail.com>
2023-04-24 03:09:55 +0400

    Create Strings.py

---
+++ Some codes/Strings.py
@@ -0,0 +1,14 @@
+print("Ayo \nWTF")
+
+phrase = "Batman Begins"
+print(phrase + " is goated")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[2])
+print(phrase.index("man"))
+print(phrase.replace("Begins", "Beyond"))
+
+
