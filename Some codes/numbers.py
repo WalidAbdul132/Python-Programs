c51beb51485127d0aa83a918d1c17a8722944977
WalidAbdul132 <abdulbuster132@gmail.com>
2023-04-23 18:24:53 +0400

    Create numbers.py

---
+++ Some codes/numbers.py
@@ -0,0 +1,22 @@
+from math import *
+print(2.897)
+print(-2344)
+print(3 + 4.5)
+print(3 * (2 + 2))
+print(27 % 4)
+
+my_num = -6
+print(my_num)
+print(str(my_num) + " is my favorite number")
+print(abs(my_num))
+print(pow(5, 2))
+print(min(50, 45))
+print(max(50, 45))
+print(round(4.7))
+print(floor(3.7))
+print(sqrt(36))
+
+
+
+
+
